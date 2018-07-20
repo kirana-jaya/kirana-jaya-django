d0bd6f7457da6423fcebbebf576e665a4e7b680e
Yosef Benny Widyokarsono <fybwid@gmail.com>
2018-07-20 13:38:43 +0800

    Adding password value

--- website/settings.py
+++ website/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
 				'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kiranajayadatabase',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
