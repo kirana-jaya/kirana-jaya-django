8673d89ad64fda24cc68fee6db09fb796d81475f
Yosef Benny Widyokarsono <fybwid@gmail.com>
2018-07-20 12:54:51 +0800

    Change database user to postgres

--- website/settings.py
+++ website/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
 				'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kiranajayadatabase',
-        'USER': 'psql',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
