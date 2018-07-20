f8a041f86b8c8eec55e0d6fa048cef2fafdd460a
Yosef Benny Widyokarsono <fybwid@gmail.com>
2018-07-20 12:00:07 +0800

    Adding Timezone

--- website/settings.py
+++ website/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
 				'ENGINE': 'django.db.backends.postgresql',
+				'TIME_ZONE': 'Asia/Chongqing',
         'NAME': 'kiranajayadatabase',
         'USER': 'psql',
         'PASSWORD': '',
